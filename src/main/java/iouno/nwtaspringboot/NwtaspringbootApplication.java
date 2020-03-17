bda23a926be2553f80ebb20a8d84a4dabe4a61cf
SZtyro <SZtyro@users.noreply.github.com>
2020-03-17 07:35:37 +0100

    Empty SpringBoot

---
+++ src/main/java/iouno/nwtaspringboot/NwtaspringbootApplication.java
@@ -0,0 +1,13 @@
+package iouno.nwtaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtaspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwtaspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iouno/nwtaspringboot/NwtaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package iouno.nwtaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwtaspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
