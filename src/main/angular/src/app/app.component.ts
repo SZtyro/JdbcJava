28d9ffbe0a5ecd3e03dfd345ce619e36fa92ddc5
SZtyro <fabixd123@gmail.com>
2021-02-13 11:35:35 +0100

    Hamburger poprawki

--- src/main/angular/src/app/app.component.ts
+++ src/main/angular/src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { MenuService } from './services/menu.service';
 import { openClose, fadeIn } from './ts/animations';
-import { Component, OnInit, AfterViewChecked, AfterContentChecked, AfterContentInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, AfterContentChecked, AfterContentInit, ViewChild } from '@angular/core';
 import { Router, RouterEvent, NavigationEnd, ActivatedRoute, Event, NavigationStart, NavigationCancel, NavigationError } from '@angular/router';
 import { HttpClientService } from './services/http-client.service';
 //import { AuthService, SocialUser } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
 import { TranslateService } from '@ngx-translate/core';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSidenav } from '@angular/material';
 import { SharedService } from './services/shared.service';
 import { Observable } from 'rxjs';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { FunctionBase } from './ts/interfaces/functionBase';
 import { ToastType } from './ts/enums/toastType';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -26,10 +27,10 @@ export class AppComponent implements OnInit {
 
 
   //sideNavExtension: boolean = false;
-  
+
   isLoading: boolean = false;
 
-  
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
   constructor(
     private httpClientService: HttpClientService,
@@ -45,14 +46,15 @@ export class AppComponent implements OnInit {
     translate.use('pl')
 
     this.shared.screenWidth = window.innerWidth;
-    window.onresize = () => {
-      this.shared.screenWidth = window.innerWidth;
-    };
+
 
   }
 
-  
-  
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.shared.screenWidth = window.innerWidth;
+  }
 
 
 
@@ -75,6 +77,10 @@ export class AppComponent implements OnInit {
 
   hasMenu() {
     return window.location.pathname != "/"
+  }
+
+  toggleSideNav(){
+    this.sidenav.toggle();
   }
 
 }
--- src/main/java/pl/sztyro/main/config/WebSecurityConfig.java
+++ src/main/java/pl/sztyro/main/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 
-                .antMatchers("/api/**").authenticated()
+                //.antMatchers("/api/**").authenticated()
                 .antMatchers("/login").permitAll()
 
 
